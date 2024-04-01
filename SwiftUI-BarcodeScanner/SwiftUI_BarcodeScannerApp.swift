12f7e2d645353c1ef7c01bff70d8891394b8d863
Natasha Radika <natashagradika@gmail.com>
2024-04-01 13:56:11 +0700

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Natasha Radika on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
